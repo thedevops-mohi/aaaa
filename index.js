0d7efb93848423f06f940ab4d14a79798df319e3
Mohammed Yahaya <daudaauwal@yahoo.co.uk>
2024-05-20 18:41:43 -0400

    just a mohi things

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/will', function (req, res) {
     res.send('{ "response": "Hello World" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works! yey!" }');
+    res.send('{ "response": " Great!, It works! yeyy!" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
