7249ea36560c5162df2a7545be10893605248710
Mohammed Yahaya <daudaauwal@yahoo.co.uk>
2024-05-22 23:02:47 -0400

    renamed directory

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Yet another test" }');
+    res.send('{ "response": "Yet another tests" }');
 });
 
 app.get('/will', function (req, res) {
